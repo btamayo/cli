e60c9d507e737d5d106a84418666b15eed200af3
Bianca Tamayo <hi@biancatamayo.me>
2017-07-30 22:02:48 -0700

    Added ansibuddy
    FullSemver: 0.1.0+0
    Branch: master

---
+++ Formula/ansible-play.rb
@@ -0,0 +1,30 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook.html
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class AnsiblePlay < Formula
+  desc "Play: Ansible-playbook helper"
+  homepage "https://github.com/btamayo/play"
+  url "https://github.com/btamayo/play/archive/0.1.0-beta.1.tar.gz"
+  sha256 "39616f1df888a8b6d6bdb4730507f966cf5bbfcd9f2cc9eb05d6c02f7e3e728b"
+  head "https://github.com/btamayo/play.git"
+
+  def install
+    opoo "WARNING: This package is still unstable and in active development"
+    bin.install "ap.sh"
+    mv bin/"ap.sh", bin/"play"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test ansible-play`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # cli
+
+This is a Homebrew tap for various side-projects from [btamayo](https://github.com/btamayo). **Please consider all of these unstable and marked as a WIP unless explicitly stated otherwise.** Unstable packages are added here only to increase dev access and portability during development.
+
+```shell
+$ brew tap btamayo/cli
+```
+
+---
+
+## play (a.k.a ansibuddy)
+
+[![Build Status](https://travis-ci.org/btamayo/play.svg?branch=master)](https://travis-ci.org/btamayo/play) [![stability-wip](https://img.shields.io/badge/stability-work_in_progress-lightgrey.svg)](https://github.com/btamayo/play)
+
+
+Repo: [https://github.com/btamayo/play](https://github.com/btamayo/play)
+Formula: [ansible-play.rb](Formula/anisble-play.rb)
+
+
+
+```shell
+$ brew update 
+$ brew install ansible-play # or brew install btamayo/cli/ansible-play
+```
+
